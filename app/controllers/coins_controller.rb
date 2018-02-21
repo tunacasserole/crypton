7b8979084fbb110d55076f0e698a178617963c65
Aaron Henderson <aaronhenderson@Aarons-MacBook-Pro.local>
2018-02-21 15:15:27 -0700

    integration

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -3,7 +3,13 @@ class CoinsController < ApplicationController
   def show
 
   end
+
   def edit
   end
+
+  def index
+    @coins = Coin.all
+  end
+
 end
 
--- db/seed/20180301000000_seed_coins.rb
+++ db/seed/20180301000000_seed_coins.rb
@@ -1,3 +1,5 @@
 Coin.create(name: 'Ethereum', description: 'Ethereum is a decentralized platform that runs smart contracts: applications that run exactly as programmed without any possibility of downtime, censorship, fraud or third-party interference.', website: 'www.ethereum.org', github: 'https://github.com/ethereum')
 Coin.create(name: 'Bitcoin')
 Coin.create(name: 'Litecoin')
+Coin.create(name: 'Tron')
+Coin.create(name: 'Cardano')
