207d10842f9f23d618d342b7e56dea18bc4aa572
Aaron Henderson <aaron@buildit.io>
2018-02-21 10:10:14 -0700

    enhancements

--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,10 @@ ActiveRecord::Schema.define(version: 20180301040000) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "seeds", id: false, force: :cascade do |t|
+    t.string "version"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
