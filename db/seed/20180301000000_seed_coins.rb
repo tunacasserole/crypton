be48816b227b4d869f1d1808f8e51457a7452a01
Aaron Henderson <aaronhenderson@Aarons-MacBook-Pro.local>
2018-02-21 22:48:37 -0700

    enhancements

--- db/seed/20180301000000_seed_coins.rb
+++ db/seed/20180301000000_seed_coins.rb
@@ -1,4 +1,4 @@
-Coin.create(name: 'Ethereum', website: 'www.ethereum.org', rating: '9', description: 'Ethereum is a decentralized platform that runs smart contracts: applications that run exactly as programmed without any possibility of downtime, censorship, fraud or third-party interference.', website: 'www.ethereum.org', github: 'https://github.com/ethereum')
+Coin.create(name: 'Ethereum', website: 'www.ethereum.org', rating: '9', description: 'Ethereum is a decentralized platform that runs smart contracts: applications that run exactly as programmed without any possibility of downtime, censorship, fraud or third-party interference.', github: 'https://github.com/ethereum')
 Coin.create(name: 'Bitcoin', website: 'www.bitcoin.org', rating:'7')
 Coin.create(name: 'Litecoin', website: 'www.litecoin.org', rating:'8')
 Coin.create(name: 'Tron', website: 'www.tron.org', rating:'6')
